264a7f4622b63b523d685315ce38727658d98355
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2025-05-19 22:24:48 +0900

    Test: 일반 승리 시 베팅 금액만큼 수익이 증가

--- src/test/java/com/example/blackjack/domain/gamer/PlayerTest.java
+++ src/test/java/com/example/blackjack/domain/gamer/PlayerTest.java
@@ -17,15 +17,35 @@ class PlayerTest {
 
         Gamer dealer = new Dealer();
         player.receiveCard(new Card("A", Pattern.SPADE));
-        player.receiveCard(new Card("K", Pattern.HEART));
+        player.receiveCard(new Card("K", Pattern.HEART));   // 21
 
         dealer.receiveCard(new Card("10", Pattern.DIAMOND));
-        dealer.receiveCard(new Card("9", Pattern.CLUB));
+        dealer.receiveCard(new Card("9", Pattern.CLUB));    // 19
 
         player.winFrom(dealer);
         dealer.loseFrom(player);
 
         assertEquals(1500, player.getMoney().getAmount());
         assertEquals(-1500, dealer.getMoney().getAmount());
+    }
+
+    @Test
+    @DisplayName("일반 승리 시 베팅 금액만큼 수익이 증가한다")
+    void testWinFrom_normalWin() {
+        Player player = new Player("aa");
+        player.betMoney(new Money(1000));
+
+        Gamer dealer = new Dealer();
+        player.receiveCard(new Card("10", Pattern.HEART));
+        player.receiveCard(new Card("9", Pattern.SPADE));   // 19
+
+        dealer.receiveCard(new Card("10", Pattern.CLUB));
+        dealer.receiveCard(new Card("8", Pattern.DIAMOND));    // 18
+
+        player.winFrom(dealer);
+        dealer.loseFrom(player);
+
+        assertEquals(1000, player.getMoney().getAmount());
+        assertEquals(-1000, dealer.getMoney().getAmount());
     }
 }
