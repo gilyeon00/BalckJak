4434868c2087f86cbca531b97fd363eca74ea97c
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2025-05-19 17:03:25 +0900

    Feat: ConsoleOutput

--- src/main/java/com/example/blackjack/view/ConsoleOutput.java
+++ src/main/java/com/example/blackjack/view/ConsoleOutput.java
@@ -1,4 +1,36 @@
 package com.example.blackjack.view;
 
+import com.example.blackjack.card.Card;
+import com.example.blackjack.domain.gamer.Dealer;
+import com.example.blackjack.domain.gamer.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ConsoleOutput {
+
+    public void printFinalCards(Dealer dealer, List<Player> players) {
+        System.out.println("\n");
+
+        System.out.println("딜러 카드: " + formatCards(dealer.getCards()) + " - 결과: " + dealer.calculateScore());
+
+        for (Player player : players) {
+            System.out.println(player.getName() + "카드: " + formatCards(player.getCards()) + " - 결과: " + player.calculateScore());
+        }
+    }
+
+    public void printProfitSummary(List<Player> players, Dealer dealer) {
+        System.out.println("\n## 최종 수익");
+        System.out.println("딜러: " + dealer.getMoney());
+
+        for (Player player : players) {
+            System.out.println(player.getName() + ": " + player.getMoney());
+        }
+    }
+
+    private String formatCards(List<Card> cards) {
+        return cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.joining(", "));
+    }
 }
